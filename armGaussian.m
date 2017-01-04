d388bb7d38e7dac8c2cd2307e2ff9fdc438c09e1
alban-pierre <alban.pierre@ens.fr>
2017-01-04 15:36:17 +0100

    Add multi gaussian arm

--- armGaussian.m
+++ armGaussian.m
@@ -1,26 +1,34 @@
 classdef armGaussian<handle
-    % % Bernoulli arm 
-    
-    properties
-		s    % Int : Current state vector (example : [0;1])
-        mean % (1*N) : Expectations of the arm for each state
-        var  % (1*N) : Variances of the arm for each state
-		A    % (N*N) : Transition matrix
-    end
-    
-    methods
-        function self = armGaussian(s, mean, var, A)
-            self.s=s; 
-            self.mean = mean;
-            self.var = var;
-            self.A = A;
-        end
-        
-        function [reward] = sample(self)
-            % return a sample from arm k
-            reward = self.mean(1,self.s) + self.var(1,self.s)*randn(1);
-			[~,self.s] = max(mnrnd(1,A(self.s,:)));
-        end
-                
-    end    
+	
+	properties
+		h	 % Int : Which state is hot
+		mean % (2*N) : Expectations of each arm for each state
+		var	 % (2*N) : Variances of each arm for each state
+		p	 % Double: Probability to stay in normal state
+		ptoH % (1*N) : Probabilities that one arm became hot
+		ptoN % (1*N) : Probabilities that each hot arm go back to normal
+	end
+	
+	methods
+		function self = armGaussian(mean, var, ptoH, ptoN)
+			self.h=0; 
+			self.mean = mean;
+			self.var = var;
+			self.p = 1-sum(ptoH);
+			self.ptoH = ptoH/sum(ptoH);
+			self.ptoN = ptoN;
+		end
+		
+		function [reward] = sample(self, s)
+			reward = self.mean(1+(h==s),s) + self.var(1+(h==s),s)*randn(1);
+			if (h == 0)
+				if (rand(1) > self.p) 
+					[~,h] = max(mnrnd(1,ptoH));
+				end
+			else
+				h = h*(rand(1)>self.ptoN(1,h));
+			end
+		end
+				
+	end	   
 end

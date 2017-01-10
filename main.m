26cc0fa5c877a18044f5474d69c7bbf9dad0685c
alban-pierre <alban.pierre@ens.fr>
2017-01-10 17:29:41 +0100

    Update main

--- main.m
+++ main.m
@@ -58,21 +58,21 @@ if (false)
     clear means;
     means{1} = [2,70];
     means{2} = [3,50];
-    means{3} = [1,80];
+    means{3} = [1,30];
     clear tr;
-    tr{1} = [0.99,0.01;0.1,0.9]';
-    tr{2} = [0.99,0.01;0.1,0.9]';
-    tr{3} = [0.99,0.01;0.1,0.9]';
-    plot_expectations(tr, means, 100);
+    tr{1} = [0.97,0.03;0.1,0.9]';
+    tr{2} = [0.95,0.05;0.2,0.8]';
+    tr{3} = [0.97,0.03;0.1,0.9]';
+    plot_expectations(tr, means, 70);
 end
 
 
 
 tmax = 10000; % KNN_UCB works efficiently for t > 2000
-ntests = 100;
+ntests = 1;
 
-allrew = zeros(4,tmax);
-%tt = time();
+allrew = zeros(5,tmax);
+tt = time();
 for i=1:ntests
     [rew, draws] = UCB(tmax, MAB1);
     allrew(1,:) = allrew(1,:) + rew;
@@ -86,15 +86,17 @@ for i=1:ntests
     allrew(2,:) = allrew(2,:) + rew;
     [rew, draws] = KNN_UCB_NEW(tmax, MAB1);
     allrew(3,:) = allrew(3,:) + rew;
+    [rew, draws] = KNN_UCB_LONG(tmax, MAB1);
+    allrew(5,:) = allrew(5,:) + rew;
     %[rew, draws] = NND_UCB(tmax, MAB1);
     %allrew(3,:) = allrew(3,:) + rew;
     %[rew, draws] = KNN_UCB_OLD(tmax, MAB1);
     %allrew(1,:) = allrew(1,:) + rew;
-    %[rew, draws] = UCB(tmax, MAB1);
+    %[rew, draws] = TS(tmax, MAB1);
     %allrew(1,:) = allrew(1,:) + rew;
     [rew, draws] = UCB_BN(tmax, MAB1);
     allrew(4,:) = allrew(4,:) + rew;
-    if(i==ntests)
+    if(false)%(i==ntests)
         figure;
         subplot(3,1,1);
         stairs(hot_real');
@@ -107,7 +109,7 @@ for i=1:ntests
     fprintf(2,'.');
 end
 disp(' ');
-%time() - tt
+time() - tt
 allrew = allrew./ntests;
 
 
@@ -115,50 +117,71 @@ allrew = allrew./ntests;
 %save('results/ucb_bn_10000_100.mat', 'allrew');
 
 figure;
-plot(1:tmax, cumsum(allrew(1,:)), '.k');
+plot(1:tmax, cumsum(allrew(1,:)), '-k', 'linewidth', 3);
 hold on;
-plot(1:tmax, cumsum(allrew(2,:)),'.m');
-plot(1:tmax, cumsum(allrew(3,:)),'.g');
-plot(1:tmax, cumsum(allrew(4,:)),'.r');
-legend('UCB','UCB\_Var', 'KNN\_UCB\_NEW', 'UCB\_BN', 'location', 'southeast');
+plot(1:tmax, cumsum(allrew(2,:)), '-r', 'linewidth', 3);
+plot(1:tmax, cumsum(allrew(3,:)), '-g', 'linewidth', 3);
+plot(1:tmax, cumsum(allrew(4,:)), '-m', 'linewidth', 3);
+plot(1:tmax, cumsum(allrew(5,:)), '-', 'Color',[.0 .5 .0], 'linewidth', 3);
+legend('UCB','VAR', 'KNN', 'BN', 'KNN\_LONG', 'location', 'southeast');
 
 
 
 if (false) % Plots stored results for each algorithm
-    % One hot arms
+           % One hot arms
     ts = load('results/ts_10000_100.mat');
     ucb = load('results/ucb_10000_100.mat');
     %knn_ucb_old = load('results/knn_ucb_old_10000_10.mat');
     knn_ucb_new = load('results/knn_ucb_new_10000_100.mat');
+    knn_ucb_long = load('results/knn_ucb_long_10000_100.mat');
     gm = load('results/gm_10000_1.mat');
-    ucb_bn = load('results/ucb_bn_10000_100.mat');
-    ucb_var = load('results/ucb_var_10000_100.mat');
+    ucb_bn = load('results/ucb_bn_2_10000_100.mat');
+    ucb_var = load('results/ucb_var_2_10000_100.mat');
     figure;
-    plot(1:10000, cumsum(ts.allrew), '.b');
+    plot(1:10000, cumsum(ts.allrew), '-b', 'linewidth', 3);
     hold on;
-    plot(1:10000, cumsum(ucb.allrew), '.k');
+    plot(1:10000, cumsum(ucb.allrew), '-k', 'linewidth', 3);
     %plot(1:10000, cumsum(knn_ucb_old.allrew), '.r');
-    plot(1:10000, cumsum(knn_ucb_new.allrew), '.g');
-    plot(1:10000, cumsum(gm.allrew), '.c');
-    plot(1:10000, cumsum(ucb_bn.allrew), '.m');
-    plot(1:10000, cumsum(ucb_var.allrew), '.r');
+    plot(1:10000, cumsum(knn_ucb_new.allrew), '-g', 'linewidth', 3);
+    plot(1:10000, cumsum(gm.allrew), '-c', 'linewidth', 1);
+    plot(1:10000, cumsum(ucb_bn.allrew), '-m', 'linewidth', 3);
+    plot(1:10000, cumsum(ucb_var.allrew), '-r', 'linewidth', 3);
+    plot(1:10000, cumsum(knn_ucb_long.allrew), '-', 'Color',[.0 .5 .0], 'linewidth', 3);
+    ylim([0, 160000]);
     title('One hot arm model');
     xlabel('Iterations');
     ylabel('Cumulative rewards');
-    legend('TS (100 runs)', 'UCB (100 runs)', 'KNN (100 runs)', 'GM (1 run)', 'BN (100 runs)', 'VAR (100 runs)', 'location', 'southeast');
+    legend('TS (100 runs)', 'UCB (100 runs)', 'KNN (100 runs)', 'GM (1 run)', 'BN (100 runs)', 'VAR (100 runs)', 'KNN\_LONG (100runs)', 'location', 'southeast');
     figure;
-    plot(1:1000, cumsum(ts.allrew(1,1:1000)), '.b');
+    plot(1:1000, cumsum(ts.allrew(1,1:1000)), '-b', 'linewidth', 3);
     hold on;
-    plot(1:1000, cumsum(ucb.allrew(1,1:1000)), '.k');
+    plot(1:1000, cumsum(ucb.allrew(1,1:1000)), '-k', 'linewidth', 3);
     %plot(1:1000, cumsum(knn_ucb_old.allrew(1,1:1000)), '.r');
-    plot(1:1000, cumsum(knn_ucb_new.allrew(1,1:1000)), '.g');
-    plot(1:1000, cumsum(gm.allrew(1,1:1000)), '.c');
-    plot(1:1000, cumsum(ucb_bn.allrew(1,1:1000)), '.m');
-    plot(1:1000, cumsum(ucb_var.allrew(1,1:1000)), '.r');
+    plot(1:1000, cumsum(knn_ucb_new.allrew(1,1:1000)), '-g', 'linewidth', 3);
+    plot(1:1000, cumsum(gm.allrew(1,1:1000)), '-c', 'linewidth', 1);
+    plot(1:1000, cumsum(ucb_bn.allrew(1,1:1000)), '-m', 'linewidth', 3);
+    plot(1:1000, cumsum(ucb_var.allrew(1,1:1000)), '-r', 'linewidth', 3);
+    plot(1:1000, cumsum(knn_ucb_long.allrew(1,1:1000)), '-', 'Color',[.0 .5 .0], 'linewidth', 3);
+    ylim([0, 16000]);
     title('One hot arm model');
     xlabel('Iterations');
     ylabel('Cumulative rewards');
-    legend('TS (100 runs)', 'UCB (100 runs)', 'KNN (100 runs)', 'GM (1 run)', 'BN (100 runs)', 'VAR (100 runs)', 'location', 'southeast');
+    legend('TS (100 runs)', 'UCB (100 runs)', 'KNN (100 runs)', 'GM (1 run)', 'BN (100 runs)', 'VAR (100 runs)', 'KNN\_LONG (100runs)', 'location', 'southeast');
+    figure;
+    plot(1:1000, cumsum(ts.allrew(1,9001:10000)), '-b', 'linewidth', 3);
+    hold on;
+    plot(1:1000, cumsum(ucb.allrew(1,9001:10000)), '-k', 'linewidth', 3);
+    %plot(1:1000, cumsum(knn_ucb_old.allrew(1,1:1000)), '.r');
+    plot(1:1000, cumsum(knn_ucb_new.allrew(1,9001:10000)), '-g', 'linewidth', 3);
+    plot(1:1000, cumsum(gm.allrew(1,9001:10000)), '-c', 'linewidth', 1);
+    plot(1:1000, cumsum(ucb_bn.allrew(1,9001:10000)), '-m', 'linewidth', 3);
+    plot(1:1000, cumsum(ucb_var.allrew(1,9001:10000)), '-r', 'linewidth', 3);
+    plot(1:1000, cumsum(knn_ucb_long.allrew(1,9001:10000)), '-', 'Color',[.0 .5 .0], 'linewidth', 3);
+    ylim([0, 16000]);
+    title('One hot arm model, 1000 last iterations over 10000');
+    xlabel('Iterations');
+    ylabel('Cumulative rewards');
+    legend('TS (100 runs)', 'UCB (100 runs)', 'KNN (100 runs)', 'GM (1 run)', 'BN (100 runs)', 'VAR (100 runs)', 'KNN\_LONG (100runs)', 'location', 'southeast');
     
     % Multiple hot arms
     ts = load('results/ts_m_10000_100.mat');
@@ -196,13 +219,14 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     % Computation time : (One hot arm)
-    % TS -------- :    7.1236s pour tmax = 10000
-    % UCB ------- :    2.1950s pour tmax = 10000
-    % KNN_UCB_OLD :   29.452s  pour tmax = 10000    % By old I mean the resize in [0-1] is made after knn
-    % KNN_UCB_NEW :   25.389s  pour tmax = 10000    % By new I mean the resize in [0-1] is made before knn
-    % GM -------- : 1000.5s    pour tmax = 10000
-    % UCB_BN ---- :    3.0577s pour tmax = 10000
-    % UCN_VAR --- :    5.1484s pour tmax = 10000
+    % TS --------- :    7.1236s pour tmax = 10000
+    % UCB -------- :    2.1950s pour tmax = 10000
+    % KNN_UCB_OLD  :   29.452s  pour tmax = 10000    % By old I mean the resize in [0-1] is made after knn
+    % KNN_UCB_NEW  :   25.389s  pour tmax = 10000    % By new I mean the resize in [0-1] is made before knn
+    % KNN_UCB_LONG :   35.853s  pour tmax = 10000    % By new I mean the resize in [0-1] is made before knn
+    % GM --------- : 1000.5s    pour tmax = 10000
+    % UCB_BN ----- :    3.0577s pour tmax = 10000
+    % UCN_VAR ---- :    5.3674s pour tmax = 10000
 
 
     % Computation time : (Several hot arm)
